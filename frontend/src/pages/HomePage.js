0eb1a1eda80e6f7d00a5753e1ea0cbd1e5879fc1
anumularoots-svg <anumula.roots@gmail.com>
2025-10-07 12:31:50 +0530

    Update HomePage.js

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,1 +1,24 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import ProductCard from "../components/ProductCard";
 
+function HomePage() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://<backend-public-ip>:5000/api/products")
+      .then(res => setProducts(res.data))
+      .catch(err => console.error(err));
+  }, []);
+
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>🌾 Welcome to RiceMart</h1>
+      <div style={{ display: "flex", flexWrap: "wrap" }}>
+        {products.map((p) => <ProductCard key={p.id} product={p} />)}
+      </div>
+    </div>
+  );
+}
+
+export default HomePage;
