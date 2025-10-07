980d7aeb0ded97877362e5ebf605a006b4047b31
anumularoots-svg <anumula.roots@gmail.com>
2025-10-07 12:32:51 +0530

    Update CartPage.js

--- frontend/src/pages/CartPage.js
+++ frontend/src/pages/CartPage.js
@@ -1,1 +1,12 @@
+import React from "react";
 
+function CartPage() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h2>🛒 Your Cart</h2>
+      <p>Items you add will appear here.</p>
+    </div>
+  );
+}
+
+export default CartPage;
