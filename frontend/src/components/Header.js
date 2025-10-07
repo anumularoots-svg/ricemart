4ae57804a57c8ccb774e6e36eb3ede6467a72074
anumularoots-svg <anumula.roots@gmail.com>
2025-10-07 12:30:35 +0530

    Update Header.js

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,1 +1,14 @@
+import React from "react";
+import { Link } from "react-router-dom";
 
+function Header() {
+  return (
+    <nav style={{ background: "#007b5e", padding: "10px", color: "white" }}>
+      <Link to="/" style={{ color: "white", margin: "10px" }}>🏠 Home</Link>
+      <Link to="/about" style={{ color: "white", margin: "10px" }}>ℹ️ About</Link>
+      <Link to="/cart" style={{ color: "white", margin: "10px" }}>🛒 Cart</Link>
+    </nav>
+  );
+}
+
+export default Header;
