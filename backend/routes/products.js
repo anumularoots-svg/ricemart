8ec143b3150d7e1a02d45920347b3451ce4df06c
anumularoots-svg <anumula.roots@gmail.com>
2025-10-07 12:20:09 +0530

    Update products.js

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -1,1 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const db = require('../db');
 
+router.get('/', (req, res) => {
+  db.query('SELECT * FROM products', (err, result) => {
+    if (err) throw err;
+    res.send(result);
+  });
+});
+
+module.exports = router;
