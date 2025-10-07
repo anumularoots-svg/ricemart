208696e5b6e4b1fc633683cc44ea77585172fc43
anumularoots-svg <anumula.roots@gmail.com>
2025-10-07 12:24:21 +0530

    Create productModel.js

---
+++ backend/models/productModel.js
@@ -0,0 +1,12 @@
+// simple model for reference
+const Product = {
+  id: Number,
+  name: String,
+  brand: String,
+  price: Number,
+  stock: Number,
+  image_url: String,
+  description: String
+};
+
+module.exports = Product;
