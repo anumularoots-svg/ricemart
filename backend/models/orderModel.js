286e7438cdd4e45f4ddd6db2cc76db6e88e26d3b
anumularoots-svg <anumula.roots@gmail.com>
2025-10-07 12:25:30 +0530

    Create orderModel.js

---
+++ backend/models/orderModel.js
@@ -0,0 +1,11 @@
+const Order = {
+  id: Number,
+  user_id: Number,
+  product_id: Number,
+  quantity: Number,
+  total_price: Number,
+  status: String,
+  order_date: Date
+};
+
+module.exports = Order;
