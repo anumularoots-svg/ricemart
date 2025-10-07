1aedaefd30af54c06057db1099de9abc7324b958
anumularoots-svg <anumula.roots@gmail.com>
2025-10-07 12:30:55 +0530

    Update Footer.js

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,1 +1,11 @@
+import React from "react";
 
+function Footer() {
+  return (
+    <footer style={{ textAlign: "center", padding: "10px", background: "#f2f2f2" }}>
+      © {new Date().getFullYear()} RiceMart | All Rights Reserved
+    </footer>
+  );
+}
+
+export default Footer;
