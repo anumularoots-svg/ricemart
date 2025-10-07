20f97b6f9510ca7f9aa1e45be6f5edddeb6d12a2
anumularoots-svg <anumula.roots@gmail.com>
2025-10-07 12:32:13 +0530

    Update ProductPage.js

--- frontend/src/pages/ProductPage.js
+++ frontend/src/pages/ProductPage.js
@@ -1,1 +1,14 @@
+import React from "react";
+import { useParams } from "react-router-dom";
 
+function ProductPage() {
+  const { id } = useParams();
+  return (
+    <div style={{ padding: "20px" }}>
+      <h2>🛍️ Product Details (ID: {id})</h2>
+      <p>Here you can show product info, description, reviews, etc.</p>
+    </div>
+  );
+}
+
+export default ProductPage;
