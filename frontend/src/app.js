c9928ac4adca426fd4255531cd9cf07f77574e9a
anumularoots-svg <anumula.roots@gmail.com>
2025-10-07 12:30:10 +0530

    Update app.js

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,1 +1,25 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import ProductPage from "./pages/ProductPage";
+import AboutPage from "./pages/AboutPage";
+import CartPage from "./pages/CartPage";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
+function App() {
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/product/:id" element={<ProductPage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/cart" element={<CartPage />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
+}
+
+export default App;
